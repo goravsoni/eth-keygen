6f0d14b71ea8a3273ca968ed78b75b9fce44d5e6
Gorav Soni <goravsoni987@gmail.com>
2022-04-03 23:39:40 -0700

    html view

--- main.js
+++
@@ -1,7 +0,0 @@
-const ethWallet = require('ethereumjs-wallet');
-/* display private key & address */
-for(let index=0; index < 1000; index++) {
-    let addressData = ethWallet['default'].generate();
-    console.log(`Private key = , ${addressData.getPrivateKeyString()}`);
-    console.log(`Address = , ${addressData.getAddressString()}`);
-}
